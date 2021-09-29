530a8dde0d591a7149b0cd7572f61bf544bb3c41
jimin <8841258@naver.com>
2021-09-29 12:27:53 +0900

    YJM / SPRING SECURITY, 2021.09.29. 12:27

--- lhmian/src/main/java/com/lh/app/member/mapper/MemberMapper.java
+++ lhmian/src/main/java/com/lh/app/member/mapper/MemberMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.lh.app.member.domain.MemberCriteria;
 import com.lh.app.signIn.domain.MemberVO;
+import com.lh.app.signIn.etc.CustomUserDetails;
 
 public interface MemberMapper {
 
@@ -18,6 +19,9 @@ public interface MemberMapper {
 	// 0928 추가
 	//단건조회
 	public MemberVO read(MemberVO vo);
+	
+	//0929 윤지민 스프링 시큐리티 로그인 커스터마이징 추가
+	public CustomUserDetails selectUserById(String username);
 	
 	//회원삭제
 	public int delete(MemberVO vo);
