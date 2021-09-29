9a0b79a6abc24f0e211b07bb44895571546e4699
jimin <8841258@naver.com>
2021-09-29 17:49:18 +0900

    YJM / SPRING SECURITY / 2021.09.29. 17:49

---
+++ lhmian/src/main/java/com/lh/app/signIn/etc/CustomLogoutSuccessHandler.java
@@ -0,0 +1,23 @@
+package com.lh.app.signIn.etc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+
+	@Override
+	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws IOException, ServletException {
+		
+		System.out.println("LogoutHandler, 로그아웃");
+		response.sendRedirect("/app");
+		
+	}
+
+}
