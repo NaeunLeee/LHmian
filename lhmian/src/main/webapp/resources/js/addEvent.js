6cc4ee7015fb605b5911630dc49d454745ebd1ef
Naeun Lee <84850264+NaeunLeee@users.noreply.github.com>
2021-09-30 22:10:26 +0900

    Update addEvent.js

--- lhmian/src/main/webapp/resources/js/addEvent.js
+++ lhmian/src/main/webapp/resources/js/addEvent.js
@@ -89,10 +89,6 @@ var newEvent = function (start, end, eventType) {
             contentType : 'application/json',
             success: function (response) {
                 //DB연동시 중복이벤트 방지를 위한
-                console.log(response);
-                $('#calendar').fullCalendar('removeEvents');
-                $('#calendar').fullCalendar('refetchEvents');
-            },
 			error : function() {
 				alert("error"); // 실패 시 처리
 			}
