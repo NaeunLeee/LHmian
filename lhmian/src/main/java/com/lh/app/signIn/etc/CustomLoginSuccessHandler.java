6086df03e147a776edd12cc6ce6314b1bb055acf
jimin <8841258@naver.com>
2021-10-10 00:50:05 +0900

    YJM / VOTE FRONT & BACK / 2021.10.10. 00:49

--- lhmian/src/main/java/com/lh/app/signIn/etc/CustomLoginSuccessHandler.java
+++ lhmian/src/main/java/com/lh/app/signIn/etc/CustomLoginSuccessHandler.java
@@ -1,22 +1,34 @@
 package com.lh.app.signIn.etc;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
+		
+		//로그인을 하는 과정에서 한번만에 로그인에 성공할 수도 있지만,
+		//실패를 한 후 로그인에 성공하는 경우도 있다.
+		//이처럼 로그인에 실패하는 상황이 한번이라도 발생한다면, 에러가 세션에 저장되어 남아있게 된다.
+		//로그인에 성공했다고 하지만 이렇게 세션에 에러가 남겨진 채로 넘어갈 수는 없다.
+		//따라서 로그인 성공 핸들러에서 에러 세션을 지우는 작업을 해줘야 한다.
+		clearAuthenticationAttributes(request);
 		
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		CustomUserDetails userDetails = (CustomUserDetails) principal;
@@ -27,12 +39,30 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
 		//admin이면 관리자 메인 페이지로 간다.
 		if (auth.equals("ADMIN")) {
 			response.sendRedirect("admin/adminPage");
-		//그 이외는 전부 홈으로
+		//그 이외
 		} else {
-			response.sendRedirect("/app");
+			RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+			RequestCache requestCache = new HttpSessionRequestCache();
+
+			SavedRequest savedRequest = requestCache.getRequest(request, response); 
+			
+			//어떤 페이지로 요청했다가 로그인 페이지로 넘어가는 경우, 이전 페이지로 간다.
+			if (savedRequest != null) {
+				String targetUrl = savedRequest.getRedirectUrl();
+				redirectStrategy.sendRedirect(request, response, targetUrl);
+			//메인 페이지에서 로그인한 경우 이전 페이지 기억값이 null이므로 메인 페이지로 리다이렉트
+			} else {
+				response.sendRedirect("/app");
+			}
 		}
 		
 		
 	}
+	
+    protected void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if(session==null) return;
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+    }
 	
 }
---
+++ lhmian/src/main/webapp/resources/js/yjmFunction/functions.js
@@ -0,0 +1,31 @@
+/**
+ * 작성자 : 윤지민
+ * 작성일 : 2021.10.10.
+ * 만든 이유 : 자꾸 똑같은거 쓰니까 답답해서...
+ */
+
+//뒤로가기		
+function goBack() {
+    window.history.back();
+}
+
+//json으로 serialize, 사용예시 : $("#WRITE_FORM").serializeObject();
+jQuery.fn.serializeObject = function() {
+    var obj = null;
+    try {
+        if (this[0].tagName && this[0].tagName.toUpperCase() == "FORM") {
+            var arr = this.serializeArray();
+            if (arr) {
+                obj = {};
+                jQuery.each(arr, function() {
+                    obj[this.name] = this.value;
+                });
+            }//if ( arr ) {
+        }
+    } catch (e) {
+        alert(e.message);
+    } finally {
+    }
+    
+    return obj;
+};
